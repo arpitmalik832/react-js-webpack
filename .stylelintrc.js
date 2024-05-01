6c2e45a865db506f48c0a8d45f32bde55445c0fe
Arpit Malik <arpit08malik2000@gmail.com>
2024-05-01 17:32:44 +0530

    added stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,43 @@
+// stylelint configuration
+// https://stylelint.io/user-guide/configuration/
+module.exports = {
+  // The standard config based on a handful of CSS style guides
+  // https://github.com/stylelint/stylelint-config-standard
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-prettier',
+    'stylelint-config-idiomatic-order',
+    'stylelint-config-hudochenkov/order',
+    'stylelint-config-clean-order/error',
+  ],
+  // stylelint plugin to sort CSS rules content with specified order
+  // https://github.com/hudochenkov/stylelint-order
+  plugins: ['stylelint-order'],
+  rules: {
+    'declaration-property-unit-allowed-list': {
+      '/^border|^padding|^gap/': ['px'],
+    },
+    'unit-allowed-list': ['%', 'px', 'ms'],
+    'color-named': 'never',
+    'function-disallowed-list': ['rgb', 'hwb', 'lch', 'hsl'],
+    'custom-property-pattern': [
+      '^[a-z][a-zA-Z0-9]+$',
+      {
+        message: x => `Expected ${x} to be in smallCamelCase`,
+      },
+    ],
+    'selector-class-pattern': [
+      '^[a-z][a-zA-Z0-9]+$',
+      {
+        message: x => `Expected ${x} to be in smallCamelCase`,
+      },
+    ],
+    'selector-id-pattern': [
+      '^[a-z][a-zA-Z0-9]+$',
+      {
+        message: x => `Expected ${x} to be in smallCamelCase`,
+      },
+    ],
+  },
+};
